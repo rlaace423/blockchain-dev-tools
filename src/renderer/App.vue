58aeeb6b3665d745758b0ed4d46bc6fbdd1615c2
Sam (Sangho Kim) <rlaace423@gmail.com>
2019-04-09 14:54:51 +0900

    INITIAL COMMIT

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'blockchain-dev-tools',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
